dd5e6e8ae0c38e457068869cb1b60d3d2969fb9c
Daniel Cruz-Castro <Dancruzcast@gmail.com>
2022-03-17 15:55:30 -0400

    Initial xcode project

---
+++ Werdd/Werdd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Werdd
+//
+//  Created by Daniel Cruz Castro on 3/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
