6b271cbb6f37e894b7f22c8d7cb19ceb8763e161
Daniel Cruz-Castro <Dancruzcast@gmail.com>
2022-03-19 12:07:09 -0400

    Removed storyboards for Programmatic UI development

--- Werdd/Werdd/ViewController.swift
+++ Werdd/Werdd/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Werdd
 //
-//  Created by Daniel Cruz Castro on 3/17/22.
+//  Created by Daniel-Cruz Castro on 3/17/22.
 //
 
 import UIKit
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
     }
 
 
