542c502c9698df86e3b5350118c9c18c26aa7f63
Daniel Cruz-Castro <Dancruzcast@gmail.com>
2022-04-02 03:00:10 -0400

    Completion of V1, rewrote code to remove implementation using UIStackViews and used UIView

--- Werdd/Werdd/ViewController.swift
+++ Werdd/Werdd/ViewController.swift
@@ -11,25 +11,30 @@ class ViewController: UIViewController {
     // Create views
     let appLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false // Automatically sets autolayout for view. set false when setting it yourself.
+        label.translatesAutoresizingMaskIntoConstraints = false // Automatically sets autolayout for view. set false when setting it yourself. Always set.
         label.text = "Werdd."
-        label.font = UIFont.systemFont(ofSize: 45, weight: .bold)
-        //label.lineBreakMode = .byWordWrapping
-        //label.numberOfLines = 1
+        label.font = UIFont.systemFont(ofSize: 47, weight: .bold)
         label.textAlignment = .left
-        //label.backgroundColor = .blue
+        //label.backgroundColor = .green
         return label
+    }()
+    
+    let cardView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(named: "CardColor")
+        view.layer.cornerRadius = 50
+        return view
     }()
     
     let wordLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Conundrum"
-        label.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+        label.text = "Programming"
+        label.font = UIFont(name: "PTSerif-Regular", size: 30)
         label.lineBreakMode = .byTruncatingTail
-        label.numberOfLines = 1
         label.textAlignment = .left
-        label.backgroundColor = .yellow
+        //label.backgroundColor = .yellow
         return label
     }()
     
@@ -37,88 +42,71 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "noun"
-        label.font = UIFont.systemFont(ofSize: 15, weight: .light)
+        label.font = UIFont(name: "PTSerif-Italic", size: 15)
         label.lineBreakMode = .byTruncatingTail
-        label.numberOfLines = 1
         label.textAlignment = .left
-        label.backgroundColor = .white
+        //label.backgroundColor = .white
         return label
     }()
     
     let definitionLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Definition goes here"
-        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.text = "Creating a sequence of instructions to enable a computer to do something"
+        label.font = UIFont(name: "PTSerif-Regular", size: 15)
         label.lineBreakMode = .byWordWrapping
         label.numberOfLines = 0
         label.textAlignment = .left
-        label.backgroundColor = .cyan
+        //label.backgroundColor = .cyan
         return label
     }()
-    
-    let titleStackView: UIStackView = {
-       let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.distribution = .fillProportionally
-        stackView.spacing = 3
-        stackView.backgroundColor = .green
-        return stackView
-    }()
-    
-    let cardStackView: UIStackView = {
-       let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.alignment = .leading
-        stackView.distribution = .fillProportionally // fill by each porportion
-        stackView.spacing = 10
-        stackView.backgroundColor = .brown
-        stackView.layer.cornerRadius = 50
-        return stackView
-    }()
 
-    // End view creation -----------------------------------------------------------------------------
+    // viewDidLoad() ------------------------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Set view to white
-        view.backgroundColor = .white
+        // Set app background color
+        view.backgroundColor = UIColor(named: "WerddColor")
         
-        // Add subviews to parent label "view"
+        // Set up UI
+        arrangeUI()
+        
+    }
+    
+    // Functions ---------------------------------------------------------------------------------------------------
+    func arrangeUI() {
+        // Function specifically to add subviews and manage layout constrains in app, meant more so to be clean
+        
         view.addSubview(appLabel)
-        view.addSubview(cardStackView)
-        cardStackView.addSubview(titleStackView)
-        cardStackView.addSubview(definitionLabel)
-        titleStackView.addSubview(wordLabel)
-        titleStackView.addSubview(typeLabel)
+        view.addSubview(cardView)
+        cardView.addSubview(wordLabel)
+        cardView.addSubview(typeLabel)
+        cardView.addSubview(definitionLabel)
         
-        // Add constraints to helloLabel/submitButton view (Size/Position)
         NSLayoutConstraint.activate([
-            // Constant = offset, "topAnchor" = sides, multiplier = percenetage of view (total?)
-            appLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            appLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            appLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            appLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.07),
+            // App title label
+            appLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            appLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            appLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor),
             
-            cardStackView.topAnchor.constraint(equalTo: appLabel.bottomAnchor, constant: 10),
-            cardStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            cardStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            cardStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.35),
+            // Card background view
+            cardView.topAnchor.constraint(equalTo: appLabel.bottomAnchor, constant: 35),
+            cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            cardView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3),
             
-            titleStackView.topAnchor.constraint(equalTo: cardStackView.topAnchor, constant: 20),
-            titleStackView.leadingAnchor.constraint(equalTo: cardStackView.leadingAnchor, constant: 20),
-            titleStackView.trailingAnchor.constraint(equalTo: cardStackView.trailingAnchor, constant: -20),
-            titleStackView.heightAnchor.constraint(equalTo: cardStackView.heightAnchor, multiplier: 0.15),  // Issues
+            // Respective word label, and type of speech label
+            wordLabel.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 20),
+            wordLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 23),
             
-            typeLabel.leadingAnchor.constraint(equalTo: wordLabel.trailingAnchor, constant: 20),
+            typeLabel.topAnchor.constraint(equalTo: wordLabel.topAnchor, constant: 17), // Would it be better to specify bottom text alignment and have typeLabel instead bottom anchor to worldlabel bottom anchor? Then location next to word label should be based on size and not respective constraints
+            typeLabel.leadingAnchor.constraint(equalTo: wordLabel.trailingAnchor, constant: 10),
+            typeLabel.trailingAnchor.constraint(lessThanOrEqualTo: cardView.trailingAnchor),
             
-            definitionLabel.topAnchor.constraint(equalTo: titleStackView.bottomAnchor, constant: 10),
-            definitionLabel.leadingAnchor.constraint(equalTo: cardStackView.leadingAnchor, constant: 20),
-            definitionLabel.trailingAnchor.constraint(equalTo: cardStackView.trailingAnchor, constant: -20),
-            definitionLabel.bottomAnchor.constraint(equalTo: cardStackView.bottomAnchor, constant: -20),
-            
+            // Definition body label
+            definitionLabel.topAnchor.constraint(equalTo: typeLabel.bottomAnchor, constant: 10),
+            definitionLabel.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: 23),
+            definitionLabel.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -15),
+            //definitionLabel.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -10),
         ])
     }
 }
